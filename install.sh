9df5376a4c39ad2f2a1389256901d0bea43ba7f2
Andres Crucitti <dasc495@gmail.com>
2017-09-06 19:02:31 -0700

    Add lucius theme

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 rm ~/.vimrc
 rm ~/.vim/plugins.vim
+rm ~/.vim/colors/lucius.vim
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/plugins.vim ~/.vim/
+ln -s `pwd`/lucius.vim ~/.vim/colors/
 if cd  ~/.vim/bundle/Vundle.vim; then git pull; else git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim; fi
 vim +PluginClean +PluginInstall +qall
