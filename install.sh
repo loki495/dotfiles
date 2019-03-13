d4529f369fcba6d178e0fe82a23b5a194feabf58
Andres Crucitti <dasc495@gmail.com>
2019-03-13 13:55:25 -0700

    Install and general tweaks

--- install.sh
+++ install.sh
@@ -7,18 +7,20 @@ rm -f ~/.vim/plugins.vim
 rm -f ~/.vim/colors/lucius.vim
 rm -rf ~/.vim/plugins/
 rm -rf ~/.vim/plugged/
-mkdir ~/.vim/plugged
-mkdir ~/.vim/backup
-mkdir ~/.vim/undo
-mkdir ~/.vim/tmp
-mkdir ~/.vim/colors
+mkdir -p ~/.vim/plugged
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/undo
+mkdir -p ~/.vim/tmp
+mkdir -p ~/.vim/colors
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/plugins.vim ~/.vim/
 ln -s `pwd`/plugins.settings.vim ~/.vim/
 ln -s `pwd`/general.vim ~/.vim/
 ln -s `pwd`/mappings.vim ~/.vim/
 ln -s `pwd`/lucius.vim ~/.vim/colors/
+ln -s `pwd`/paragold-ora.vim ~/.vim/colors/
+ln -s `pwd`/paragold.vim ~/.vim/colors/
 cd $CURRENT_DIR
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugClean +PlugInstall +qall
+vim +PlugClean +'PlugInstall --sync' +qall
