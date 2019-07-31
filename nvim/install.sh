7c65475fb320c98445ff7adcd96b1d78c82fa96e
Andres Crucitti <dasc495@gmail.com>
2019-07-31 10:57:41 -0700

    Add nvim install script

---
+++ nvim/install.sh
@@ -0,0 +1,11 @@
+rm nvim.appimage* -f
+curl -sL install-node.now.sh/lts | sudo bash -s -- -y
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+curl -sfLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
+chmod a+x ./nvim.appimage
+./nvim.appimage --appimage-extract
+sed '/nvim/d' ~/.bashrc > ~/.bashrc
+echo 'alias nvim="/home/ec2-user/dotfiles/nvim/squashfs-root/usr/bin/nvim -u /home/ec2-user/dotfiles/nvim/init.vim"' >> ~/.bashrc
+source ~/.bashrc
+echo 'source ~/dotfiles/nvim/init.vim' > ~/.config/nvim/init.vim
+nvim +PlugClean +'PlugInstall --sync' +qall
