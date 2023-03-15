13272713b23f22b98892276f1ba2dd4f41771e0e
Andres Crucitti <dasc495@gmail.com>
2023-03-15 05:12:24 -0700

    NVIM: tweaks for installing, aliases and remaps

--- install.sh
+++ install.sh
@@ -46,12 +46,18 @@ curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
 
 echo "VIM done"
+echo "VIM: :PlugInstall"
+
+~/dotfiles/install_neovim_appimage.sh
 
 rm -rf ~/.config/nvim
 ln -s ~/dotfiles/nvim ~/.config/nvim
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 echo "NVIM done"
+echo "NVIM: :PackerSync"
 
 echo "DONE"
 
-echo "VIM: :PlugInstall"
-echo "NVIM: :PackerSync"
--- install_neovim_appimage.sh
+++ install_neovim_appimage.sh
@@ -10,24 +10,15 @@
     curl -LO https://github.com/neovim/neovim/releases/download/$CHANNEL/nvim.appimage
     chmod u+x nvim.appimage
 
-	# mkdir -p $HOME/.local/bin
-	# cp nvim.appimage $HOME/.local/bin/
-	# $HOME/.local/bin/nvim.appimage
-	chmod a+x nvim.appimage
 	./nvim.appimage --appimage-extract
 	mv squashfs-root ~
+    mv ~/squashfs-root ~/nvim
 
 	if [ ! -d ~/.config/nvim ]; then
 		cd ~/.config && git clone https://github.com/dilawar/nvim --recursive
 	fi
-	echo "Make sure ~/squashfs-root/usr/bin is in path"
-
-	# From https://stackoverflow.com/a/28021305/1805129
-	LINE='alias vim=$HOME/squashfs-root/usr/bin/nvim'
-	FILE=$HOME/.bashrc
-	grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 	source ~/.bashrc
 )
 
-echo "Delete ~/.cache/*.appimage if you like now"
+rm -f ~/.cache/nvim.appimage
