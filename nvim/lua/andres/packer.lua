0929bf8538eb01205833416a12a16ca13a466025
Andres Crucitti <dasc495@gmail.com>
2023-04-16 02:31:32 -0700

    NVIM: add telescope+fzf, and tresitter-context

--- nvim/lua/andres/packer.lua
+++ nvim/lua/andres/packer.lua
@@ -28,6 +28,16 @@ return require('packer').startup(function(use)
 
     use { 'nvim-lua/plenary.nvim' }
 
+    use("nvim-treesitter/nvim-treesitter-context");
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
 	--use {
         --'VonHeikemen/lsp-zero.nvim',
         --branch = 'v1.x',
