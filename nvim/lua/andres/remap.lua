f748535d198b6581d76c8e2653b16fc1bf75af5c
Andres Crucitti <dasc495@gmail.com>
2023-08-26 04:42:47 -0700

    NVIM: update disabling arrow keys / pastetoggle

--- nvim/lua/andres/remap.lua
+++ nvim/lua/andres/remap.lua
@@ -1,46 +1,40 @@
 vim.g.mapleader = ","
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+-- Clear highlighted search results with ENTER
 vim.keymap.set("n", "<CR>", [[{-> v:hlsearch ? ":nohl<CR>" : "<CR>"}()]], { silent = true, expr = true })
 
+-- Ctr-C to escape insert
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Check if 4 below necessary / useful
+--       paste without overwriting register map?
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
+-- Quickerror / location list navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Substitute word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Source current file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+-- Launch Navbuddy
 vim.keymap.set({"n","x","i"}, "<leader>ee", function ()
     local navbuddy = require("nvim-navbuddy")
     navbuddy.open()
 end)
 
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
--- Disable arrow keys
-map('', '<up>', '<nop>')
-map('', '<down>', '<nop>')
-map('', '<left>', '<nop>')
-map('', '<right>', '<nop>')
-
-map('n', '<F2>', ':set invpaste paste?<CR>')
-vim.opt.pastetoggle = '<F2>'
-
+-- Disable arrow key
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
