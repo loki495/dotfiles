0d8edf5d17e445486a4550003bb2703a25d44790
Andres Crucitti <dasc495@gmail>
2024-02-25 11:17:39 -0800

    NVim: update codeium key bindings

--- nvim/lua/andres/lazy.lua
+++ nvim/lua/andres/lazy.lua
@@ -103,8 +103,11 @@ require("lazy").setup({
             "hrsh7th/nvim-cmp",
         },
         config = function()
-            require("codeium").setup({
-        })
+            require("codeium").setup(
+                {
+                    vim.g.codeium_no_map_tab = 1,
+                    vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+                })
         end
     },
 
