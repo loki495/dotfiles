815bd630ed0657eb88ce49becf84e6af27ad2d43
Andres Crucitti <dasc495@gmail.com>
2024-02-28 11:07:54 -0800

    Nvim: updates and fixes

--- nvim/lua/andres/remap.lua
+++ nvim/lua/andres/remap.lua
@@ -13,11 +13,13 @@ vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- Quickerror / location list navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- Tab / Quickerror / location list navigation
+vim.keymap.set("n", "<leader>[", ":tabprev<CR>", { silent = true })
+vim.keymap.set("n", "<leader>]", ":tabnext<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { silent = true })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { silent = true })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { silent = true })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { silent = true })
 
 -- Substitute word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
