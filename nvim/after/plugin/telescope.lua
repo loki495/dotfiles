e1f0a75e2cf70645782027d96c1f1dfc91645423
Andres Crucitti <dasc495@gmail.com>
2024-12-22 14:03:47 -0800

    NVim: add telescope live grep args plugin

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,11 @@
+local telescope = require('telescope')
+telescope.load_extension("live_grep_args")
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>;', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
--- nvim/lua/andres/lazy.lua
+++ nvim/lua/andres/lazy.lua
@@ -42,7 +42,17 @@ require("lazy").setup({
             --"--branch=stable", -- latest stable release
 
         -- or                            , branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
+        dependencies = {
+            {
+                'nvim-lua/plenary.nvim'
+            },
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim" ,
+                -- This will not install any breaking changes.
+                -- For major updates, this must be adjusted manually.
+                version = "^1.0.0",
+            },
+        }
     },
 
     {
