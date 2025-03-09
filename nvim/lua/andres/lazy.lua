edb599c6be4093d487794226b1b650e9ade68b29
Andres Crucitti <dasc495@gmail.com>
2025-03-09 14:28:34 -0700

    NVim add some snacks

--- nvim/lua/andres/lazy.lua
+++ nvim/lua/andres/lazy.lua
@@ -129,6 +129,29 @@ require("lazy").setup({
         -- Optional dependencies
         dependencies = { { "echasnovski/mini.icons", opts = {} } },
         -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    },
+
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            dashboard = { enabled = true },
+            --dim = { enabled = true },
+            indent = { enabled = true },
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
+            scope = { enabled = true },
+            scroll = { enabled = true },
+            terminal = { enabled = true },
+            --statuscolumn = { enabled = true },
+            --words = { enabled = true },
+        },
     }
 
 })
