e6f21fa22aff35efb7a28f71fa4b0b85117bf9aa
Andres Crucitti <dasc495@gmail.com>
2025-10-23 19:07:16 -0700

    nvim: add mappings for save/source, close file, run file

--- nvim/lua/andres/remap.lua
+++ nvim/lua/andres/remap.lua
@@ -28,18 +28,17 @@ vim.keymap.set("n", "<C-l>", ":b#<CR>", { silent = true })
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Source current file
-vim.keymap.set("n", "<leader><leader>", function()
+vim.keymap.set({"n","i"}, "<leader><leader>", function()
     local file = vim.api.nvim_buf_get_name(0)
 
-  if file:match("^" .. vim.fn.expand("~/.config/nvim/lua/")) then
-    -- If the file is in your config/lua folder, source it
-    vim.cmd("source " .. vim.fn.fnameescape(file))
-    print("Sourced: " .. file)
-  else
-    -- Otherwise, just save the file
-    vim.cmd("write")
-    print("Saved: " .. file)
-  end
+    if file:match("^" .. vim.fn.expand("~/.config/nvim/lua/")) or file:match("^" .. vim.fn.expand("~/dotfiles/nvim/lua/")) then
+        -- If the file is in your config/lua folder, source it
+        vim.cmd("source " .. vim.fn.fnameescape(file))
+        print("Sourced: " .. file)
+    else
+        -- Otherwise, just save the file
+        vim.cmd("write")
+    end
 end, { desc = "Source or save file" })
 
 -- Disable arrow key
@@ -83,17 +82,37 @@ vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 vim.keymap.set('n', '<leader>w', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 vim.keymap.set("n", "<leader>e", '<cmd>lua vim.lsp.buf.code_action()<CR>', { silent = true })
 
-vim.keymap.set("n", "Q", function()
-  local nofile = vim.api.nvim_buf_get_option(0, "buftype") == "nofile"
+vim.keymap.set('n', '<leader>.',  function()
+    local file = vim.fn.expand("%:p")     -- full path to current file
+  local ext  = vim.fn.expand("%:e")     -- file extension
 
-  vim.print(vim.api.nvim_buf_get_name(0))
-  vim.print(vim.api.nvim_buf_get_name(0))
-  if nofile then
-    vim.cmd("q")
-  elseif vim.api.nvim_buf_get_name(0) ~= "" then
-    vim.cmd("wq")
+  local cmd
+  if ext == "php" then
+    cmd = "php " .. vim.fn.fnameescape(file)
+  elseif ext == "py" then
+    cmd = "python " .. vim.fn.fnameescape(file)
+  elseif ext == "js" then
+    cmd = "node " .. vim.fn.fnameescape(file)
   else
-    vim.cmd("q!")
+    print("No runner defined for ." .. ext .. " files")
+    return
+  end
+
+  -- open in a split terminal
+  vim.cmd("split term://" .. cmd)
+end, { desc = "Run current PHP file in terminal" })
+
+vim.keymap.set("n", "Q", function()
+  local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+  local name    = vim.api.nvim_buf_get_name(0)
+
+  -- buffers you can't/shouldn't save
+  if buftype ~= "" then
+    vim.cmd("q")  -- just close terminal, help, quickfix, etc.
+  elseif name ~= "" then
+    vim.cmd("wq") -- normal file buffer
+  else
+    vim.cmd("q!") -- unnamed buffer
   end
 end, { noremap = true, silent = true })
 
@@ -131,8 +150,8 @@ vim.keymap.set("n", "<leader>vv", ToggleVSplitWidth, { desc = "Toggle VSplit Wid
 
 vim.keymap.set("n", "<leader>t", ':tabnew<CR>', { silent = true })
 
-vim.keymap.del("n", "<C-W><C-D>")
-vim.keymap.del("n", "<C-W>d")
+pcall(vim.keymap.del, "n", "<C-W><C-D>")
+pcall(vim.keymap.del, "n", "<C-W>d")
 vim.keymap.set("n", "<C-W>", '<C-W><C-W>', { desc = "Toggle VSplit Width (90%/50%)" })
 
 local run = require("andres.run_in_vsplit")
