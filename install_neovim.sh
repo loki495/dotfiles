8492869ddec0e8ac82bff4b106f25add7e88590c
Andres Crucitti <dasc495@gmail.com>
2025-11-05 14:17:46 -0800

    nvim: allow install nvim for older glibc

--- install_neovim.sh
+++ install_neovim.sh
@@ -5,7 +5,13 @@
     rm -rf nvim-linux64.tar.gz
     rm -rf ~/nvim-linux64/
     rm -rf ~/nvim/
-    wget "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+    if [ "$1" = "old" ]; then
+        echo "Older GLibC installing..."
+        wget "https://github.com/neovim/neovim-releases/releases/download/nightly/nvim-linux-x86_64.tar.gz"
+    else
+        echo "Newer GLibC installing..."
+        wget "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+    fi
     tar xvf nvim-linux-x86_64.tar.gz
     mv ~/nvim-linux-x86_64/ ~/nvim
     rm -rf nvim-linux-x86_64.tar.gz
