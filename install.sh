03813a3132dc7b2c952f8f5d9fbce9dd92c2da2c
Andres Crucitti <dasc495@gmail.com>
2025-12-30 20:28:50 -0800

    readme and install tweaks

--- install.sh
+++ install.sh
@@ -1,25 +1,54 @@
+#!/usr/bin/env bash
+set -e # Exit immediately if a command exits with a non-zero status.
+
+# Function to check for command existence
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Check for essential commands
+echo "Checking for required commands..."
+REQUIRED_COMMANDS=("readlink" "dirname" "mv" "ln" "source" "curl" "wget" "tar" "mkdir" "rm")
+for cmd in "${REQUIRED_COMMANDS[@]}"; do
+  if ! command_exists "$cmd"; then
+    echo "Error: Required command '$cmd' is not installed. Please install it and try again."
+    exit 1
+  fi
+done
+echo "All required commands found."
+
 # Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
+SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-# BASH
-mv ~/.bashrc ~/.bashrc.old
-ln -s $SCRIPTPATH/bash/bashrc ~/.bashrc
-source ~/.backrc
-echo "BASH done"
+echo "------------------------------------"
+echo "Setting up BASH dotfiles..."
+mv ~/.bashrc ~/.bashrc.old || true # Use || true to prevent error if file doesn't exist
+ln -s "$SCRIPTPATH/bash/bashrc" ~/.bashrc
+source ~/.bashrc # Fixed typo here
+echo "BASH dotfiles done."
 
-mv ~/.dircolors ~/.dircolors.old
-ln -s $SCRIPTPATH/bash/dircolors ~/.dircolors
-echo "BASH done"
+echo "------------------------------------"
+echo "Setting up dircolors..."
+mv ~/.dircolors ~/.dircolors.old || true
+ln -s "$SCRIPTPATH/bash/dircolors" ~/.dircolors
+echo "dircolors done."
 
-mv ~/.gitconfig ~/.gitconfig.old
-ln -s $SCRIPTPATH/git/.gitconfig ~/.gitconfig
-echo "GIT done"
+echo "------------------------------------"
+echo "Setting up GIT dotfiles..."
+mv ~/.gitconfig ~/.gitconfig.old || true
+ln -s "$SCRIPTPATH/git/.gitconfig" ~/.gitconfig
+echo "GIT dotfiles done."
 
-ln -s $SCRIPTPATH/.mcphost ~/.mcphost
+echo "------------------------------------"
+echo "Setting up .mcphost symlink..."
+ln -s "$SCRIPTPATH/.mcphost" ~/.mcphost || true # Symlink might already exist
+echo ".mcphost done."
 
-# VIM
+echo "------------------------------------"
+echo "Setting up VIM dotfiles..."
+# Cleanup old vim configurations
 rm -f ~/.vimrc
 rm -f ~/.vim/plugins.settings.vim
 rm -f ~/.vim/general.vim
@@ -29,44 +58,72 @@ rm -f ~/.vim/colors/lucius.vim
 rm -f ~/.vim/colors/scratch.vim
 rm -rf ~/.vim/plugins/
 rm -rf ~/.vim/plugged/
-unlink ~/.vim/colors/ 2>/dev/null
+unlink ~/.vim/colors/ 2>/dev/null || true # If symlink exists, unlink. || true to ignore error if not.
 mkdir -p ~/bin
 mkdir -p ~/.vim/plugged
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/tmp
-ln -s $SCRIPTPATH/vim/vimrc ~/.vimrc
-ln -s $SCRIPTPATH/vim/colors/ ~/.vim/colors
-touch ~/.vimrc.local
+ln -s "$SCRIPTPATH/vim/vimrc" ~/.vimrc
+ln -s "$SCRIPTPATH/vim/colors/" ~/.vim/colors
+touch ~/.vimrc.local # Ensure this file exists for local customizations
 
+echo "Downloading vim-plug..."
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
+echo "VIM dotfiles done."
+echo "  - After running this script, open Vim and run :PlugInstall to install plugins."
 
-echo "VIM done"
-echo "- run :PlugInstall"
-
+echo "------------------------------------"
+echo "Setting up .config symlinks..."
 mkdir -p ~/.config
 
 # Garuda stuff
-rm -rf ~/.config/wireplumber
-ln -s $SCRIPTPATH/.config/wireplumber ~/.config/wireplumber
-rm -rf ~/.config/waybar
-ln -s $SCRIPTPATH/.config/waybar ~/.config/waybar
-rm -rf ~/.config/hypr
-ln -s $SCRIPTPATH/.config/hypr ~/.config/hypr
-rm -rf ~/.config/fish
-ln -s $SCRIPTPATH/.config/fish ~/.config/fish
+rm -rf ~/.config/wireplumber || true
+ln -s "$SCRIPTPATH/.config/wireplumber" ~/.config/wireplumber
+rm -rf ~/.config/waybar || true
+ln -s "$SCRIPTPATH/.config/waybar" ~/.config/waybar
+rm -rf ~/.config/hypr || true
+ln -s "$SCRIPTPATH/.config/hypr" ~/.config/hypr
+rm -rf ~/.config/fish || true
+ln -s "$SCRIPTPATH/.config/fish" ~/.config/fish
+echo ".config symlinks done."
 
-# NVIM
-rm -rf ~/.config/nvim
-ln -s ~/dotfiles/nvim ~/.config/nvim
 
-rm ~/.local/share/nvim/site/pack/packer -rf
+echo "------------------------------------"
+echo "Neovim Application Installation"
+echo "Do you want to install Neovim for the current user or globally?"
+echo "  1) User-local installation (\$HOME/.local/bin/nvim)"
+echo "  2) Global installation (/usr/local/bin/nvim - requires sudo)"
+read -rp "Please enter 1 or 2: " nvim_choice
 
-~/dotfiles/install_neovim.sh
+NVIM_INSTALL_FLAG=""
+if [ "$nvim_choice" == "1" ]; then
+    NVIM_INSTALL_FLAG="--user"
+    echo "Proceeding with user-local Neovim application installation."
+    "$SCRIPTPATH/install_neovim.sh" "$NVIM_INSTALL_FLAG"
+elif [ "$nvim_choice" == "2" ]; then
+    NVIM_INSTALL_FLAG="--global"
+    echo "Proceeding with global Neovim application installation (will prompt for sudo password)."
+    sudo "$SCRIPTPATH/install_neovim.sh" "$NVIM_INSTALL_FLAG"
+else
+    echo "Invalid choice or no choice made. Skipping Neovim application installation."
+    echo "You can run '$SCRIPTPATH/install_neovim.sh --user' or 'sudo $SCRIPTPATH/install_neovim.sh --global' manually later."
+fi
 
-echo "NVIM done"
+echo "------------------------------------"
+echo "Neovim Configuration Link"
+# Link the Neovim configuration directory from your dotfiles
+rm -rf ~/.config/nvim || true # Remove old symlink or directory
+ln -s "$SCRIPTPATH/nvim" ~/.config/nvim # Create new symlink to dotfiles nvim config
+echo "Neovim configuration linked to ~/.config/nvim."
+echo "NVIM setup done."
 
 echo
-echo "DONE"
 
+echo "===================================="
+echo "All dotfiles setup complete!"
+echo "Remember to:"
+echo "  - I you want to use Vim and its plugins, open Vim and run :PlugInstall."
+echo "  - Ensure '$HOME/.local/bin' is in your PATH if you chose user-local Neovim installation."
+echo "===================================="
--- install_neovim.sh
+++ install_neovim.sh
@@ -1,18 +1,66 @@
 #!/usr/bin/env bash
-(
-    cd ~
+set -e # Exit immediately if a command exits with a non-zero status.
 
-    rm -rf nvim-linux64.tar.gz
-    rm -rf ~/nvim-linux64/
-    rm -rf ~/nvim/
-    if [ "$1" = "old" ]; then
-        echo "Older GLibC installing..."
-        wget "https://github.com/neovim/neovim-releases/releases/download/nightly/nvim-linux-x86_64.tar.gz"
-    else
-        echo "Newer GLibC installing..."
-        wget "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+INSTALL_TYPE=""
+
+if [ "$1" == "--global" ]; then
+    INSTALL_TYPE="global"
+elif [ "$1" == "--user" ]; then
+    INSTALL_TYPE="user"
+else
+    echo "Error: You must specify either --user or --global for Neovim installation."
+    echo "Usage: $0 [--user|--global]"
+    exit 1
+fi
+
+# Use a temporary directory for downloads
+TMP_DIR=$(mktemp -d)
+cd "$TMP_DIR"
+
+echo "Fetching latest Neovim release information..."
+# Use curl to follow redirects and get the latest release URL
+LATEST_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/neovim/neovim/releases/latest)
+LATEST_TAG=$(basename "$LATEST_URL")
+
+echo "Downloading Neovim ($LATEST_TAG)..."
+wget --quiet "https://github.com/neovim/neovim/releases/download/$LATEST_TAG/nvim-linux64.tar.gz"
+
+echo "Extracting archive..."
+tar xzf nvim-linux64.tar.gz
+
+if [ "$INSTALL_TYPE" == "global" ]; then
+    echo "Installing Neovim globally..."
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "Error: Global installation requires root privileges. Please run with sudo."
+        exit 1
     fi
-    tar xvf nvim-linux-x86_64.tar.gz
-    mv ~/nvim-linux-x86_64/ ~/nvim
-    rm -rf nvim-linux-x86_64.tar.gz
-)
+    # Ensure target directories exist
+    mkdir -p /opt/nvim
+    # Remove existing installation
+    rm -rf /opt/nvim/*
+    # Move files
+    mv nvim-linux64/* /opt/nvim/
+    # Create symlink
+    ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
+    echo "Neovim installed globally."
+    echo "Executable is at /usr/local/bin/nvim"
+else # INSTALL_TYPE is "user"
+    echo "Installing Neovim for current user..."
+    # Ensure target directories exist
+    mkdir -p "$HOME/.local/share/nvim"
+    mkdir -p "$HOME/.local/bin"
+    # Remove existing installation
+    rm -rf "$HOME/.local/share/nvim/*"
+    # Move files
+    mv nvim-linux64/* "$HOME/.local/share/nvim/"
+    # Create symlink
+    ln -sf "$HOME/.local/share/nvim/bin/nvim" "$HOME/.local/bin/nvim"
+    echo "Neovim installed for the current user."
+    echo "Executable is at $HOME/.local/bin/nvim"
+    echo "Please ensure '$HOME/.local/bin' is in your PATH."
+fi
+
+echo "Cleaning up..."
+rm -rf "$TMP_DIR"
+
+echo "Neovim installation complete."
